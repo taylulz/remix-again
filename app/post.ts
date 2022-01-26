0f2d74d0086190465ed2f7d3b00a251b02cd9567
taylor d <taylor.delph@eyecuelab.com>
2022-01-25 19:52:07 -0800

    update getPosts to read from file system

--- app/post.ts
+++ app/post.ts
@@ -1,18 +1,29 @@
+import path from "path";
+import fs from "fs/promises";
+import parseFrontMatter from "front-matter";
+
 export type Post = {
   slug: string;
   title: string;
 };
 
-export function getPosts() {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post"
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You"
-    }
-  ];
-  return posts;
+// relative to server output, not source
+const postsPath = path.join(__dirname, "..", "posts")
+
+export async function getPosts() {
+  const dir = await fs.readdir(postsPath);
+  return Promise.all(
+    dir.map(async filename => {
+      const file = await fs.readFile(
+        path.join(postsPath, filename)
+      );
+      const { attributes } = parseFrontMatter(
+        file.toString()
+      );
+      return {
+        slug: filename.replace(/\.md$/, ""),
+        title: attributes.title
+      };
+    })
+  )
 }
