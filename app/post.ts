6de869b0967b9cd15be2dde6ab1e1318a1a19c3f
taylor d <taylor.delph@eyecuelab.com>
2022-01-25 20:05:08 -0800

    implement invariant

--- app/post.ts
+++ app/post.ts
@@ -1,14 +1,24 @@
 import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
 
 export type Post = {
   slug: string;
   title: string;
 };
 
-// relative to server output, not source
-const postsPath = path.join(__dirname, "..", "posts")
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
+const postsPath = path.join(__dirname, "..", "posts");
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
 
 export async function getPosts() {
   const dir = await fs.readdir(postsPath);
@@ -20,10 +30,14 @@ export async function getPosts() {
       const { attributes } = parseFrontMatter(
         file.toString()
       );
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
       return {
         slug: filename.replace(/\.md$/, ""),
         title: attributes.title
       };
     })
-  )
+  );
 }
