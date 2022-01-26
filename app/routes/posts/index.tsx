0602fcc104fb161e347a3137285799dc97af9380
taylor d <taylor.delph@eyecuelab.com>
2022-01-25 19:32:40 -0800

    add types and generic for loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,12 @@
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
+
+export type Post = {
+  slug: string;
+  title: string;
+};
 
 export const loader = () => {
-  return [
+  const posts: Post[] = [
     {
       slug: "my-first-post",
       title: "My First Post"
@@ -11,14 +16,22 @@ export const loader = () => {
       title: "A Mixtape I Made Just For You"
     }
   ];
+  return posts;
 };
 
 export default function Posts() {
-  const posts = useLoaderData();
+  const posts = useLoaderData<Post[]>();
   console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
